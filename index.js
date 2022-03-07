f3af338febead8cc1d86e3af9bebcab2b103d87e
Edgar <franky9202@gmail.com>
2022-03-07 14:05:43 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const name = "Aki"
+function introduction(name){
+    return(`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name, language = Ember.js){
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name, language = "JavaScript"){ 
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
